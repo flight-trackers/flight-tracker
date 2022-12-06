dd602c359f23eee5fa20e4452e93b62bcd895075
Jake Fenn <jake.fenn@raytheon.co.uk>
2022-12-06 16:48:37 +0000

    added auth to opensky api call

--- server/src/routes/all-flights/index.ts
+++ server/src/routes/all-flights/index.ts
@@ -2,8 +2,14 @@ import axios from "axios";
 import { FastifyPluginAsync } from "fastify";
 
 const allFlights: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  const options = {
+    auth: {
+      username: process.env.USERNAME as string,
+      password: process.env.PASSWORD as string
+  }
+}
   fastify.get("/", async function (request, reply) {
-    const resp = await axios.get("https://opensky-network.org/api/states/all");
+    const resp = await axios.get("https://opensky-network.org/api/states/all", options);
 
     const data = resp.data;
 
