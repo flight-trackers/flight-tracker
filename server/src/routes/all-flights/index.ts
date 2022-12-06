b7045a4540119fa51cdf1e0dab62ab1d25925ab5
James <james.beswick@raytheon.co.uk>
2022-12-06 16:18:03 +0000

    added get all flights middleware

--- server/src/routes/all-flights/index.ts
+++ server/src/routes/all-flights/index.ts
@@ -1,19 +1,11 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 import { FastifyPluginAsync } from "fastify";
 
 const allFlights: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
-    const options: AxiosRequestConfig = {
-      auth: {
-        username: "example",
-        password: "pass",
-      },
-    };
+    const resp = await axios.get("https://opensky-network.org/api/states/all");
 
-    const data = await axios.get(
-      "https://opensky-network.org/api/states/all",
-      options
-    );
+    const data = resp.data;
 
     return data;
   });
