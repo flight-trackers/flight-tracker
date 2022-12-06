c877a2c1f341b826b014944eb45496cfb2d24e03
Matthew Neil <119592540+matt-xia@users.noreply.github.com>
2022-12-06 15:55:45 +0000

    finished navbar functionality

--- flight-tracker-client/src/components/NavBar.tsx
+++ flight-tracker-client/src/components/NavBar.tsx
@@ -1,10 +1,112 @@
-import { Box, AppBar, Toolbar } from "@mui/material"
+import React, { useState } from 'react'
+import { Box, AppBar, Toolbar, TextField, IconButton, Button, Menu, MenuItem } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import "../components.css"
 
-function NavBar(){
+function NavBar() {
+    const [searchInput, setSearchInput] = useState<string>()
+    const [username, setUsername] = useState<string>()
+    const [password, setPassword] = useState<string>()
+    const [theme, setTheme] = useState<boolean>(true)
+    const [loggedIn, setLoggedIn] = useState<boolean>(true)
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const open = Boolean(anchorEl)
+
+    const menuOpen = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const menuClose = () => {
+        setAnchorEl(null);
+    }
+
+    const updateTheme = () => {
+        setTheme(prev => !prev)
+    }
+
+    const loginUser = () => {
+        setLoggedIn(prev => !prev)
+    }
+    
+    const logoutUser = () => {
+        setLoggedIn(prev => !prev)
+    }
+    
     return (
-        <Box sx={{flexGrow:1}}>
+        <Box sx={{flexGrow: 1}}>
             <AppBar position="static">
-                <Toolbar>
+                <Toolbar className='toolbar'>
+                    <div className='toolbar-left'>
+                        <IconButton onClick={menuOpen}
+                                    sx={{ color: 'white' }}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu anchorEl={anchorEl}
+                                open={open}
+                                onClose={menuClose}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left'
+                                }}
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left'
+                                }}>
+                            {loggedIn ? 
+                                <>
+                                    <TextField size="small"
+                                                placeholder='Username'
+                                                sx={{ backgroundColor: 'white', 
+                                                        borderRadius: 1, 
+                                                        border: 'none',
+                                                        margin: '0 10px 0 10px'
+                                                }}
+                                                value={username}
+                                                onChange={e => setUsername(e.target.value)} />
+                                    <TextField size="small"
+                                                placeholder='Password'
+                                                sx={{ backgroundColor: 'white', 
+                                                        borderRadius: 1, 
+                                                        border: 'none',
+                                                        margin: '0 10px 10px 0'
+                                                }}
+                                                value={password}
+                                                onChange={e => setPassword(e.target.value)} />
+                                    <MenuItem onClick={loginUser}
+                                                sx={{ margin: '0 10px' }}>Log In</MenuItem>
+                                </>
+                            :
+                                <>
+                                    <MenuItem sx={{ margin: '0 10px' }}>Settings</MenuItem>
+                                    <MenuItem onClick={updateTheme}
+                                            sx={{ margin: '0 10px' }}>
+                                        {theme ? 'Dark' : 'Light'}
+                                    </MenuItem>
+                                    <MenuItem onClick={logoutUser}
+                                                sx={{ margin: '0 10px' }}>Log Out</MenuItem>
+                                </>
+                            }
+
+                        </Menu>
+                        <p className='toolbar-user'>Username</p>
+                        <img src="https://via.placeholder.com/50" className='toolbar-avatar' />
+                    </div>
+                    <div className='toolbar-right'>
+                        <TextField className='toolbar-search'
+                                    size="small"
+                                    placeholder='Search'
+                                    sx={{ backgroundColor: 'white', 
+                                            borderRadius: 1, 
+                                            border: 'none'
+                                    }}
+                                    value={searchInput}
+                                    onChange={e => setSearchInput(e.target.value)} />
+                        <IconButton sx={{ color: 'white' }}>
+                            <SearchIcon />
+                        </IconButton>
+                    </div>
                 </Toolbar>
             </AppBar>
         </Box>
