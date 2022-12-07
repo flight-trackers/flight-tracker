40384864f8baa98ad6fdd54e6707680d8e73318d
James <james.beswick@raytheon.co.uk>
2022-12-07 08:53:39 +0000

    fixed naming convention and formatting

--- server/src/routes/all-flights/index.ts
+++ server/src/routes/all-flights/index.ts
@@ -1,18 +1,21 @@
 import axios from "axios";
 import { FastifyPluginAsync } from "fastify";
-import { IflightData } from "src/interfaces/IflightData";
+import { IFlightData } from "src/interfaces/IflightData";
 
 const allFlights: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   const options = {
     auth: {
       username: process.env.USERNAME as string,
-      password: process.env.PASSWORD as string
-  }
-}
+      password: process.env.PASSWORD as string,
+    },
+  };
   fastify.get("/", async function (request, reply) {
-    const resp = await axios.get("https://opensky-network.org/api/states/all", options);
+    const resp = await axios.get(
+      "https://opensky-network.org/api/states/all",
+      options
+    );
 
-    const data:IflightData = resp.data;
+    const data: IFlightData = resp.data;
 
     return data;
   });
