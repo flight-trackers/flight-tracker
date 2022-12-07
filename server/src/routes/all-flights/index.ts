75d0a6a1982d98f2e4d6fc58a5de6a6b1226b0f7
Jake Fenn <jake.fenn@raytheon.co.uk>
2022-12-07 14:06:03 +0000

    formatted api call data, and deleted readme

--- server/src/routes/all-flights/index.ts
+++ server/src/routes/all-flights/index.ts
@@ -1,6 +1,7 @@
+import { IFlightData } from 'src/interfaces/IflightData';
+import { IState } from 'src/interfaces/IflightData';
 import axios from "axios";
 import { FastifyPluginAsync } from "fastify";
-import { IFlightData } from "src/interfaces/IflightData";
 
 const allFlights: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   const options = {
@@ -15,9 +16,36 @@ const allFlights: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
       options
     );
 
-    const data: IFlightData = resp.data;
 
-    return data;
+    const mapData:IState[] = resp.data.states.map((flight:any[]) => {
+      return ({
+        icao24: flight[0],
+        callsign: flight[1],
+        origin_country: flight[2],
+        time_position: flight[3],
+        last_contact: flight[4],
+        longitude: flight[5] ?? 'unknown',
+        latitude: flight[6] ?? 'unknown',
+        baro_altitude: flight[7] ?? 'unknown',
+        on_ground: flight[8],
+        velocity: flight[9] ?? 'unknown',
+        true_track: flight[10] ?? 'unknown',
+        vertical_rate: flight[11] ?? 'unknown',
+        sensors: flight[12] ?? 'unknown',
+        geo_altitude: flight[13] ?? 'unknown',
+        squawk: flight[14] ?? 'unknown',
+        spi: flight[15],
+        position_source: flight[16],
+        category: flight[17] ?? 'unknown'
+      })
+    })
+
+    const allFlightData:IFlightData = {
+      time: resp.data.time,
+      states: [...mapData]
+    }
+
+    return allFlightData;
   });
 };
 
