c1d36c0dced69bfd631909c54828a7ba17c79a51
Jake Fenn <jake.fenn@raytheon.co.uk>
2022-12-08 10:02:40 +0000

    removed unusedButton import

--- flight-tracker-client/src/App.tsx
+++ flight-tracker-client/src/App.tsx
@@ -1,24 +1,24 @@
-import { Button, createTheme, CssBaseline, ThemeProvider } from "@mui/material"
-import { useState } from "react"
-import NavBar from "./components/NavBar"
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [theme, setTheme] = useState<boolean>(true)
+  const [theme, setTheme] = useState<boolean>(true);
   const updateTheme = () => {
-    setTheme(prev => !prev)
-  }
+    setTheme((prev) => !prev);
+  };
   let themes = createTheme({
     palette: {
-      mode: theme ? "light" : "dark"
-    }
+      mode: theme ? "light" : "dark",
+    },
   });
-  
+
   return (
     <ThemeProvider theme={themes}>
-      <CssBaseline/>
-      <NavBar changeTheme={updateTheme} currTheme={theme}/>
+      <CssBaseline />
+      <NavBar changeTheme={updateTheme} currTheme={theme} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
