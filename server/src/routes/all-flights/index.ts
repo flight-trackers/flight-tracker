451b8eb7b59dc3b9e16dcff047a2b0eb54417f6d
James <james.beswick@raytheon.co.uk>
2022-12-08 09:22:26 +0000

    changed interface file name, prettified

--- server/src/routes/all-flights/index.ts
+++ server/src/routes/all-flights/index.ts
@@ -1,5 +1,5 @@
-import { IFlightData, IOpenSkyRawData } from 'src/interfaces/IflightData';
-import { IState } from 'src/interfaces/IflightData';
+import { IFlightData, IOpenSkyRawData } from "src/interfaces/IOpenSkyData";
+import { IState } from "src/interfaces/IOpenSkyData";
 import axios from "axios";
 import { FastifyPluginAsync } from "fastify";
 
@@ -16,34 +16,35 @@ const allFlights: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
       options
     );
 
+    const mapData: IState[] = resp.data.states.map(
+      (flight: IOpenSkyRawData[]) => {
+        return {
+          icao24: flight[0],
+          callsign: flight[1],
+          origin_country: flight[2],
+          time_position: flight[3],
+          last_contact: flight[4],
+          longitude: flight[5] ?? "unknown",
+          latitude: flight[6] ?? "unknown",
+          baro_altitude: flight[7] ?? "unknown",
+          on_ground: flight[8],
+          velocity: flight[9] ?? "unknown",
+          true_track: flight[10] ?? "unknown",
+          vertical_rate: flight[11] ?? "unknown",
+          sensors: flight[12] ?? "unknown",
+          geo_altitude: flight[13] ?? "unknown",
+          squawk: flight[14] ?? "unknown",
+          spi: flight[15],
+          position_source: flight[16],
+          category: flight[17] ?? "unknown",
+        };
+      }
+    );
 
-    const mapData:IState[] = resp.data.states.map((flight:IOpenSkyRawData[]) => {
-      return ({
-        icao24: flight[0],
-        callsign: flight[1],
-        origin_country: flight[2],
-        time_position: flight[3],
-        last_contact: flight[4],
-        longitude: flight[5] ?? 'unknown',
-        latitude: flight[6] ?? 'unknown',
-        baro_altitude: flight[7] ?? 'unknown',
-        on_ground: flight[8],
-        velocity: flight[9] ?? 'unknown',
-        true_track: flight[10] ?? 'unknown',
-        vertical_rate: flight[11] ?? 'unknown',
-        sensors: flight[12] ?? 'unknown',
-        geo_altitude: flight[13] ?? 'unknown',
-        squawk: flight[14] ?? 'unknown',
-        spi: flight[15],
-        position_source: flight[16],
-        category: flight[17] ?? 'unknown'
-      })
-    })
-
-    const allFlightData:IFlightData = {
+    const allFlightData: IFlightData = {
       time: resp.data.time,
-      states: [...mapData]
-    }
+      states: [...mapData],
+    };
 
     return allFlightData;
   });
